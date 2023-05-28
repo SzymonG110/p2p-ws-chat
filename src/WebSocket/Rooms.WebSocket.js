feb4c6a3100b91c3decdcfde302b8a855610fd31
SzymonG110 <gornikowskiszymon@gmail.com>
2023-05-28 10:58:46 +0200

    remove build from ssrc source (missclick while building XD)

--- src/WebSocket/Rooms.WebSocket.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.queue = void 0;
-const queue = [];
-exports.queue = queue;
-const rooms = new Map();
-exports.default = rooms;
